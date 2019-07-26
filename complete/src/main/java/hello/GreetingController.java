c805ba3890445c1cf800aabed64ec4d0664e01f4
Nemo <nemo@monalabs.io>
2019-07-26 13:37:00 +0300

    testing mona as java spring bean

--- complete/src/main/java/hello/GreetingController.java
+++ complete/src/main/java/hello/GreetingController.java
@@ -1,6 +1,10 @@
 package hello;
 
 import java.util.concurrent.atomic.AtomicLong;
+
+import io.monalabs.client.MonaClient;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +17,18 @@ public class GreetingController {
 
     @RequestMapping("/greeting")
     public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+        System.out.println("greeting started");
+        /*String host = "going.monalabs.io";
+        int port = 24224;
+        String monaUserId = "nemo_test99";*/
+        ApplicationContext context = new AnnotationConfigApplicationContext(MonaConfig.class);
+        MonaClient monaClient = context.getBean("monaClient", MonaClient.class);
+        String msg = String.format("{'experiment': 'f1', 'name': '%s'}", name);
+        String arcClass = "ARC_CLASS_99";
+        //try (MonaClient monaClient = new MonaClient.Builder(host, port, monaUserId).build()) {
+        monaClient.exportStandalone(arcClass, msg);
+        //}
+        System.out.println(String.format("sent %s to mona with arc class %s with bean!", msg, arcClass));
         return new Greeting(counter.incrementAndGet(),
                             String.format(template, name));
     }
