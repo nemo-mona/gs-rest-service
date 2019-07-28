6e41a4646ffbbb1c0a9685113d73aea63156bca3
Nemo <nemo@monalabs.io>
2019-07-28 04:35:16 +0300

    Programatically supressing messages - works for applications running slf4j and logback (like springframework's default)

--- complete/src/main/java/hello/MonaConfig.java
+++ complete/src/main/java/hello/MonaConfig.java
@@ -1,6 +1,11 @@
 package hello;
 
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.Logger;
 import io.monalabs.client.MonaClient;
+import org.komamitsu.fluency.fluentd.ingester.sender.NetworkSender;
+import org.komamitsu.fluency.fluentd.ingester.sender.RetryableSender;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,8 +14,18 @@ public class MonaConfig {
 
     @Bean(destroyMethod="close", name="monaClient")
     public MonaClient getMonaClient() {
+        //ContextSelector selector = ContextSelectorStaticBinder.getSingleton().getContextSelector();
+        //LoggerContext loggerContext = selector.getLoggerContext();
+        //loggerContext.
+        String host = "going.monalabs.io";
+        Logger networkSender = (Logger) LoggerFactory.getLogger(NetworkSender.class);
+        Logger retryableSender = (Logger) LoggerFactory.getLogger(RetryableSender.class);
+        networkSender.setLevel(Level.OFF);
+        retryableSender.setLevel(Level.OFF);
+        int port = 24224;
+        System.out.println(String.format("Returning mona client with address %s:%d", host, port));
         return new MonaClient.Builder(
-                "gong.monalabs.io", 24224, "nemo_test99").build();
+                host, port, "nemo_test99").build();
     }
 
 }
